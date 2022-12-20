8e955872c5a62e0e85138cfafe11f59385414c55
Desktop <dinleo11@gmail.com>
2022-12-20 15:11:44 +0900

    mjs

--- dist/index.mjs
+++ dist/index.mjs
@@ -2,7 +2,7 @@ export const handler = async(event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from zip!'),
+        body: JSON.stringify('Hello from zip!!!!'),
     };
     return response;
 };
