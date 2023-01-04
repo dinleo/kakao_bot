b8f1264402b19bb39210094157bda0b65350523d
Desktop <dinleo11@gmail.com>
2023-01-04 22:48:00 +0900

    toString

--- index.mjs
+++ index.mjs
@@ -22,7 +22,7 @@ export const handler = async (event) => {
     }
     const response = {
         statusCode: 200,
-        body: json,
+        body: json.toString(),
     };
     return response;
 };
