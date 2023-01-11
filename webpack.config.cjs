0eea142eae2c7b54cac921bcfb24fa38c47104f7
Desktop <dinleo11@gmail.com>
2023-01-11 15:00:44 +0900

    cjs

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -15,7 +15,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         publicPath: '/dist/',
-        filename: 'index.mjs'
+        filename: 'index.cjs'
     },
     // 모듈 적용
     module: {
