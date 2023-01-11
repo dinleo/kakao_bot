ecad4c066025ce5a49700f03dd7debaf5c3f11e1
Desktop <dinleo11@gmail.com>
2023-01-11 15:56:19 +0900

    바벨적용

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -11,5 +11,19 @@ module.exports = {
     },
     target: 'node',
     devtool: 'source-map',
-    // Work around "Error: Can't resolve 'pg-native'"
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/, // 적용 시킬 확장자
+                include: path.join(__dirname), // 찾을 폴더
+                exclude: /(node_modules)|(dist)/, // 제외할 폴더
+                use: { // 적용할 모듈
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    }
+                }
+            }
+        ]
+    },
 };
