d4a6eb2c3d28e8c3b82f791f8cd679c07a5f0d49
Desktop <dinleo11@gmail.com>
2023-01-11 15:37:15 +0900

    aws 용 config

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,37 +1,15 @@
-// package.json 이 type module 이라면 .cjs 로 해야한다.
-const path = require('path')
-// const JsConfigWebpackPlugin = require('js-config-webpack-plugin');
-
+const path = require('path');
 
 module.exports = {
     mode: 'development',
-    // 노드환경과 유사하게 컴파일 해준다. ( 기본값 'web' )
-    target: 'node',
-    // main file
-    entry: {
-        main: './index.js'
-    },
-    // build file
+    entry: './test.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        publicPath: '/dist/',
-        filename: 'index.cjs'
+        filename: 'index.cjs',
+        library: 'index',
+        libraryTarget: 'commonjs2',
     },
-    // 모듈 적용
-    module: {
-        rules: [
-            {
-                test: /\.m?js$/, // 적용 시킬 확장자
-                include: path.join(__dirname), // 찾을 폴더
-                exclude: /(node_modules)|(dist)/, // 제외할 폴더
-                use: { // 적용할 모듈
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                        plugins: ['@babel/plugin-proposal-class-properties']
-                    }
-                }
-            }
-        ]
-    },
+    target: 'node',
+    devtool: 'source-map',
+    // Work around "Error: Can't resolve 'pg-native'"
 };
