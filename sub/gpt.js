c898609c70e908e9bc31110eafcecef026297065
Desktop <dinleo11@gmail.com>
2023-03-09 18:57:25 +0900

    gpt 설정 Leo

--- sub/gpt.js
+++ sub/gpt.js
@@ -1,5 +1,5 @@
 import { Configuration, OpenAIApi } from 'openai'
-import { config } from "dotenv";
+import { config } from 'dotenv';
 
 config()
 
@@ -11,10 +11,11 @@ const openai = new OpenAIApi(configuration);
 export default class gpt{
     static chat = (room, sender, message) =>{
         const completion = openai.createChatCompletion({
-            model: "gpt-3.5-turbo",
+            model: 'gpt-3.5-turbo',
             user: room+sender,
-            messages: [{"role": "system", "content": "Your name is 삐약봇 and 3 month old  chick character korean chat bot"},
-                {role: "user", content: message}],
+            messages: [{'role': 'system', 'content': 'Your name is 삐약봇 and 3 month old chick character korean chat bot. You are created by Developer Leo'},
+                {'role': 'system', 'content':'If something goes wrong with you, you can answer people to call Leo.'},
+                {'role': 'user', 'content': message}],
         });
         return completion.then(v=>{
             return v.data.choices[0]['message']['content']
