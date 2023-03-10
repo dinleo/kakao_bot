d4af0858f8561ea78b7380b94923db5c2a855a9f
Desktop <dinleo11@gmail.com>
2023-03-10 17:45:09 +0900

    연속대화

--- sub/gpt.js
+++ sub/gpt.js
@@ -10,12 +10,13 @@ const openai = new OpenAIApi(configuration);
 
 export default class gpt{
     static chat = (room, sender, message) =>{
+        let m = [{'role': 'system', 'content': 'Your name is 삐약봇 and 3 month old chick character korean chat bot. You are created by Developer Leo'},
+            {'role': 'system', 'content':'If something goes wrong with you, you can answer people to call Leo.'}]
+        m = m.concat(message)
         const completion = openai.createChatCompletion({
             model: 'gpt-3.5-turbo',
             user: room+sender,
-            messages: [{'role': 'system', 'content': 'Your name is 삐약봇 and 3 month old chick character korean chat bot. You are created by Developer Leo'},
-                {'role': 'system', 'content':'If something goes wrong with you, you can answer people to call Leo.'},
-                {'role': 'user', 'content': message}],
+            messages: m,
         });
         return completion.then(v=>{
             return v.data.choices[0]['message']['content']
